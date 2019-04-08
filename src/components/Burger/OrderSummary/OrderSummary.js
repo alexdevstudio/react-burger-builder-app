6b7d66bf2ee73e0f64b04e92d7d35ac37c13b2f3
Alex <info@alexdev.gr>
2019-04-09 02:22:16 +0300

    App optimisiation

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react'
 //import classes from './OrderSummary.module.css'
 import Aux from '../../../hoc/Auxiliary'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = props => {
+class OrderSummary extends Component {
+  componentDidUpdate () {
+    console.log('[Order summary] Did Update');
+  }
+    render () {
+      const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+          return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
+        })
+        return (
+          <Aux>
+            <h3>Your Order</h3>
+            <p>Delicious burger with the following ingredients</p>
+            <ul>
+            {ingredientSummary}
+            </ul>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}$</strong>  </p>
+            <p>Continue to Checkout ?</p>
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+          </Aux>
 
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
-    })
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>Delicious burger with the following ingredients</p>
-      <ul>
-      {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}$</strong>  </p>
-      <p>Continue to Checkout ?</p>
-      <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
 
-
-  )
+        )
+  }
 }
 
-export default orderSummary
+export default OrderSummary
